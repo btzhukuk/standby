810eb24c1a0c9c5e22836b2e4a5ff79ee027e3ec
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-02-26 16:54:19 +0300

    feat: to cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,15 +14,7 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'docs',
-      assets: 'docs',
-      fallback: null,
-      precompress: false,
-      strict: true
-    }),
+    adapter: adapter(),
   },
 };
 
